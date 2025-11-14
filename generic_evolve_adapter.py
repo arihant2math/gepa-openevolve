4a04f8dd3ab80ce5d4552943e649047e92755075
Angela He <angelah0503@gmail.com>
2025-11-13 18:24:55 -0800

    Generic Evolve Adapter implementation

---
+++ generic_evolve_adapter.py
@@ -0,0 +1,106 @@
+from __future__ import annotations
+
+import importlib.util
+import os
+import shutil
+import sys
+import tempfile
+from types import ModuleType
+from typing import Any, Callable, Optional
+from pathlib import Path
+import importlib
+
+from gepa import EvaluationBatch, GEPAAdapter
+import yaml
+
+class EvaluationStrategy:
+    def evaluate(self, program_path: str) -> list:
+        raise NotImplementedError
+
+class DefaultEvaluationStrategy(EvaluationStrategy):
+    def __init__(self, path: Path):
+        self.path = path
+        self.module = self.load_function()
+
+    def load_function(self) -> Callable:
+        # import
+        spec = importlib.util.spec_from_file_location(self.path)
+        module = importlib.util.module_from_spec(spec)
+        sys.modules[self.path.stem] = module
+        spec.loader.exec_module(module)
+        if not hasattr(module, "evaluate"):
+            raise AttributeError(f"evaluate function not found in {self.path}")
+        return getattr(module, "evaluate")
+
+    def evaluate(self, program_path: str) -> list:
+        return self.module.evaluate(program_path)
+
+class CascadeEvaluationStrategy(EvaluationStrategy):
+    def __init__(self, path: Path, cascade_thresholds: list[float]):
+        self.path = path
+        self.stages = self.load_stages()
+        self.cascade_thresholds = cascade_thresholds
+
+    def load_stages(self) -> dict[str, Callable]:
+        required_stages = ["evaluate_stage1"]
+        possible_stages = ["evaluate_stage2", "evaluate_stage3"]
+        stages = {}
+        # import
+        spec = importlib.util.spec_from_file_location(self.path)
+        module = importlib.util.module_from_spec(spec)
+        sys.modules[self.path.stem] = module
+        spec.loader.exec_module(module)
+        for stage in required_stages:
+            if hasattr(module, stage):
+                stages[stage] = getattr(module, stage)
+            else:
+                raise AttributeError(f"Required stage {stage} not found in {self.path}")
+        for stage in possible_stages:
+            if hasattr(module, stage):
+                stages[stage] = getattr(module, stage)
+        return stages
+
+    def evaluate(self, program_path: str) -> list:
+        stage1 = self.stages["evaluate_stage1"]
+        merged_results = stage1(program_path)
+        # TODO: implement cascading logic based on thresholds
+
+class EvolveAdapter(GEPAAdapter):
+    def __init__(self, path: Path, cascade=False, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.cascade = cascade
+        self.path = path
+        self.config = yaml.safe_load(open(path / "config.yaml", "r"))
+        self.evaluator_path = path / "evaluator.py"
+        self.evaluation_strategy = CascadeEvaluationStrategy(self.evaluator_path) if cascade else DefaultEvaluationStrategy(self.evaluator_path)
+        self.temp_env_path = Path(tempfile.mkdtemp())
+        # Initialized with uv
+        os.system(f"uv init .", cwd=self.temp_env_path)
+        # Create a new temporary python environment using uv, initialize it with os.path.join(path_to_open_evolve_example, "requirements.txt")
+        if not (self.path / "requirements.txt").exists():
+            raise FileNotFoundError(f"Requirements file not found at {path / 'requirements.txt'}")
+        os.system(f"uv pip install -r {self.path / 'requirements.txt'}", cwd=self.temp_env_path)
+
+    def evaluate(self, candidate: dict, inputs: list) -> list:
+        # candidate = {'code': '# Evolve-Block -Start ... # Evolve-Block end'}
+        # write the code to a temporary file
+        with open("temp_code.py", "w") as f:
+            f.write(candidate['code'])
+        # run the code
+        # run the evaluate method with the temporary file
+        eval_out = self.evaluation_strategy.evaluate(candidate['code'])
+        # Post process this eval_out to return a list of scores and feedback
+        scores = [eval_out['score'] for eval_out in eval_out]
+        return scores
+
+    def make_reflective_dataset(self, candidate: dict, inputs: list, trajectories: list) -> list:
+        if not self.config['evaluator']['enable_artifacts']:
+            return super().make_reflective_dataset(candidate, inputs, trajectories)
+        else: 
+            # TODO: replicate openevolve behavior
+            return super().make_reflective_dataset(candidate, inputs, trajectories)
+
+    def propose_new_texts(self, candidate: dict, inputs: list, trajectories: list) -> dict:
+        return super().propose_new_texts(candidate, inputs, trajectories)
+        # Use the llm config from config.yaml to propose new texts
+        # Use the prompt.system_prompt from config.yaml to propose new texts
